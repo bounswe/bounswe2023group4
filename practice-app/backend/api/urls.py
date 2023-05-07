55117a02af2d22643930d0ff57e0cc60440d1d06
EmreBatuhan <93476131+EmreBatuhan@users.noreply.github.com>
2023-05-07 13:23:56 +0300

    Update api/urls.py
    A small name change

--- practice-app/backend/api/urls.py
+++ practice-app/backend/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path,include
 from api.views import country_views
-from api.routers.country_router import router
+from api.routers.country_router import country_router
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(country_router.urls)),
     path("countries/post/", country_views.CreateCountry.as_view()),
     path("countries/clearall/", country_views.ClearCountries.as_view()),
     path("countries/collect/", country_views.CollectData.as_view()),
