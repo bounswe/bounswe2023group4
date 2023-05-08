5510e141f45b479f409eeb7df9ce58d45b56cafd
Ali Nasra <52269552+AliNasra@users.noreply.github.com>
2023-05-08 10:11:27 +0300

    Removing some comments

--- practice-app/backend/api/routers/poll_router.py
+++ practice-app/backend/api/routers/poll_router.py
@@ -1,5 +1,5 @@
 from api.viewsets.poll_viewset import PollViewSet
 from rest_framework import routers
-# Create your models here.
+
 poll_router = routers.DefaultRouter()
 poll_router.register('poll',PollViewSet)
--- practice-app/backend/api/urls.py
+++ practice-app/backend/api/urls.py
@@ -4,10 +4,10 @@ from api.routers.country_router import country_router
 from api.routers.poll_router import poll_router
 
 urlpatterns = [
+    path('', include(poll_router.urls)),
     path('', include(country_router.urls)),
     path("countries/new/", country_views.CreateCountry.as_view()),
     path("countries/clearall/", country_views.ClearCountries.as_view()),
-    path("countries/collect/", country_views.CollectData.as_view()),
-    path('', include(poll_router.urls)),
+    path("countries/collect/", country_views.CollectData.as_view()),  
     path('poll/create/', poll_views.createPoll.as_view())
 ]
