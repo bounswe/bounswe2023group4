8fc0d3a87a315e41a9b7e542daebd60a84d6b2fa
Enes Furkan Arslan <enfurars@gmail.com>
2023-05-09 18:55:04 +0300

    Setup for my own API
    Router, Serializer, and viewsets are written in django

---
+++ practice-app/backend/api/serializers/weather_serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from api.models.weather_model import Weather
+
+class WeatherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Weather
+        fields = ('country','name','description')
