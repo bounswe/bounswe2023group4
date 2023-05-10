c997a13d4fcbb76eb1762704ace6da8b909e7029
AliAlperenSonmez <ali.sonmez@boun.edu.tr>
2023-05-11 01:37:13 +0300

    changed config/urls file, performed migrations

--- practice-app/backend/config/urls.py
+++ practice-app/backend/config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='api/', permanent=False)),
     path('admin/', admin.site.urls),
-    path('api/',include("api.urls"))
+    path('api/', include('api.urls')),
 ]
