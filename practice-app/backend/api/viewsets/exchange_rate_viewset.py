841bc4e936efad62be0a6273a2bf82d39ead5aa9
AliAlperenSonmez <ali.sonmez@boun.edu.tr>
2023-05-11 00:14:41 +0300

    Updated exchange rate views and created the viewset

---
+++ practice-app/backend/api/viewsets/exchange_rate_viewset.py
@@ -0,0 +1,17 @@
+from api.serializers.exchange_rate_serializer import ExchangeRateSerializer
+from api.models.exchange_rate_model import ExchangeRate
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+
+class ExchangeRateViewSet(viewsets.ModelViewSet):
+    queryset = ExchangeRate.objects.all()
+    serializer_class = ExchangeRateSerializer
+    filterset_fields = ('name' , 'gdp')
+
+    @action(methods=['get'],detail=False)
+    def data(self,request):
+        exchange_rates = ExchangeRate.objects.all()
+        serializer = self.get_serializer_class()(exchange_rates, many=True)
+        return Response(serializer.data)
