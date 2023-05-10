fc4b79957ddf55315d8440e38bddae6a6d7921f7
EmreBatuhan <93476131+EmreBatuhan@users.noreply.github.com>
2023-05-10 16:08:54 +0300

    Update model
    Added null=False lines

--- practice-app/backend/api/models/sport_model.py
+++ practice-app/backend/api/models/sport_model.py
@@ -3,15 +3,15 @@ from django.db import models
 
 class League_Season(models.Model): 
     id = models.AutoField(primary_key= True)
-    name = models.CharField(max_length=50)
-    country = models.CharField(max_length=50)
-    season = models.IntegerField(default= 0)
+    name = models.CharField(max_length=50,null=False)
+    country = models.CharField(max_length=50,null=False)
+    season = models.IntegerField(default= 0,null=False)
     
 class Standing(models.Model):
-    league_id = models.ForeignKey(League_Season,default=None,on_delete=models.CASCADE)
-    rank = models.IntegerField(default=0)
-    team = models.CharField(max_length=50)
-    points = models.IntegerField(default=-1)
+    league_id = models.ForeignKey(League_Season,default=None,on_delete=models.CASCADE,null=False)
+    rank = models.IntegerField(default=0,null=False)
+    team = models.CharField(max_length=50,null=False)
+    points = models.IntegerField(default=0,null=False)
 
 
 
