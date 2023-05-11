519f512f2bc1f357c428d52afb54924311b40492
Ali Nasra <52269552+AliNasra@users.noreply.github.com>
2023-05-12 02:08:13 +0300

    Fully restoring the old version of the repo

--- practice-app/backend/api/routers/country_router.py
+++ practice-app/backend/api/routers/country_router.py
@@ -1,5 +1,5 @@
 from api.viewsets.country_viewset import CountryViewSet
 from rest_framework import routers
-#Adding the country router
+
 country_router = routers.DefaultRouter()
 country_router.register('countries',CountryViewSet)
