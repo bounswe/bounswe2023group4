1d411775f1a166eb16e852e5a2f95037531805a3
Ali Nasra <52269552+AliNasra@users.noreply.github.com>
2023-05-12 01:58:08 +0300

    Restoring the old files in the API folder

--- practice-app/backend/api/urls.py
+++ practice-app/backend/api/urls.py
@@ -4,10 +4,13 @@ from rest_framework import routers
 from api.routers.poll_router import poll_router
 from rest_framework.schemas import get_schema_view 
 from django.views.generic import TemplateView
+from api.views import country_views
+from api.routers.country_router import country_router
 
 
 router = routers.DefaultRouter()
 router.registry.extend(poll_router.registry)
+router.registry.extend(country_router.registry)
 
 urlpatterns = [
     path('api_schema',get_schema_view(title = 'API Schema', description = 'API documentation'), name = 'api_schema'),
---
+++ practice-app/backend/api/viewsets/country_viewset.py
@@ -0,0 +1,17 @@
+from api.serializers.country_serializer import CountrySerializer
+from api.models.country_model import Country
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    filterset_fields = ('name' , 'gdp')
+
+    @action(methods=['get'],detail=False)
+    def data(self,request):
+        countries = Country.objects.all()
+        serializer = self.get_serializer_class()(countries, many=True)
+        return Response(serializer.data)
