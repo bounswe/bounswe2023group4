78c27f3f7eaeef0dcc13c13affd9918c35166a9b
Enes Furkan Arslan <enfurars@gmail.com>
2023-05-12 17:37:18 +0300

    few updates for tests

--- practice-app/backend/api/tests/test_weather.py
+++ practice-app/backend/api/tests/test_weather.py
@@ -61,14 +61,14 @@ class TestUrls(TestCase):
         assert(len(Weather.objects.all())==2)
 
 
-    def test_weather_view_url_is_resolved3(self):
+    def test_weather_get(self):
         # Collect view that collects data from 3rd party APIs
         url = '/api/weather_collect/'
         response = self.client.get(url)
 
         assert(len(Weather.objects.all())>=2)
 
-    def test_weather_view_url_is_resolved2(self):
+    def test_weather_clear(self):
         # Clearall view that clears all data from weathers table
         url = '/api/weather_clearall/'
         response = self.client.delete(url)
