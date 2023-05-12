72a23c4ebcdb39eff1121f3840f95b86ba71b06d
aemresafak <59789023+aemresafak@users.noreply.github.com>
2023-05-12 22:01:47 +0300

    changed settings

--- practice-app/backend/config/settings.py
+++ practice-app/backend/config/settings.py
@@ -26,7 +26,6 @@ load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-Movie_Secret_Key = os.getenv('Movie_Secret_Key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
