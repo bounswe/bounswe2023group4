c9cc78de03f30d548261c98c76a5898016b9649b
YigitSekerci <yigitsekerci6174@gmail.com>
2023-05-12 16:13:02 +0300

    implement country api hooks

--- practice-app/frontend/api/emre-batuhan-goc/useCountries.ts
+++ practice-app/frontend/api/emre-batuhan-goc/useCountries.ts
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query'
+import { useMutation, useQuery } from 'react-query'
 import { Country } from './types'
 
 const fetchCountries = async () => {
@@ -6,12 +6,70 @@ const fetchCountries = async () => {
   const response = await fetch(`${apiBaseUrl}/countries/`, {
     mode: 'cors',
   })
-  console.log(response.body)
   return response.json()
 }
 
-const useCountries = () => {
-  return useQuery<Country[]>('countries', fetchCountries)
+type UseCountriesProps = {
+  config?: any
 }
 
-export { useCountries }
+const useCountries = (props: UseCountriesProps) => {
+  const { config } = props
+  return useQuery<Country[]>('countries', fetchCountries, config)
+}
+
+type UseMutationCountriesProps = {
+  config?: any
+}
+
+const refreshCountries = async () => {
+  const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
+  const response = await fetch(`${apiBaseUrl}/countries_collect/`, {
+    mode: 'cors',
+    method: 'get',
+  })
+  return response.json()
+}
+
+const useRefreshCountries = (props: UseMutationCountriesProps) => {
+  const { config } = props
+  return useMutation('refreshCountries', refreshCountries, config)
+}
+
+const deleteCountries = async () => {
+  const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
+  await fetch(`${apiBaseUrl}/countries_clearall/`, {
+    mode: 'cors',
+    method: 'delete',
+  })
+}
+
+const useDeleteCountries = (props: UseMutationCountriesProps) => {
+  const { config } = props
+  return useMutation('deleteCountries', deleteCountries, config)
+}
+
+const createCountry = async (country: Country) => {
+  const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
+  const response = await fetch(`${apiBaseUrl}/countries/`, {
+    mode: 'cors',
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(country),
+  })
+  return response.json()
+}
+
+const useCreateCountry = (props: UseMutationCountriesProps) => {
+  const { config } = props
+  return useMutation('createCountry', createCountry, config)
+}
+
+export {
+  useCountries,
+  useRefreshCountries,
+  useDeleteCountries,
+  useCreateCountry,
+}
