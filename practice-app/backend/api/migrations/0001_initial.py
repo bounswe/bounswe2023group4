10a025d7dd6ea885419fe4f74ab88f9820d1bf89
Ali Nasra <52269552+AliNasra@users.noreply.github.com>
2023-05-12 18:16:37 +0300

    Modifications to migrations

--- practice-app/backend/api/migrations/0001_initial.py
+++ practice-app/backend/api/migrations/0001_initial.py
@@ -6,17 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     operations = [
-        migrations.CreateModel(
-            name='Country',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=50)),
-                ('capital', models.CharField(max_length=50)),
-                ('population', models.IntegerField(default=0)),
-                ('gdp', models.IntegerField(default=0)),
-            ],
-        ),
-    
+   
         migrations.CreateModel(
             name='Movie',
             fields=[
