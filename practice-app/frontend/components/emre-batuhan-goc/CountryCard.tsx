61dbea1a1cd3faef0a4e2286ca4ade0c36a33057
YigitSekerci <yigitsekerci6174@gmail.com>
2023-05-12 16:13:32 +0300

    implement country page

--- practice-app/frontend/components/emre-batuhan-goc/CountryCard.tsx
+++ practice-app/frontend/components/emre-batuhan-goc/CountryCard.tsx
@@ -4,10 +4,7 @@ const CountryCard = (props: { country: Country }) => {
   const { country } = props
 
   return (
-    <div
-      className="border rounded-lg p-8 flex flex-col gap-3
-      bg-gradient-to-br from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%"
-    >
+    <div className="border rounded-lg p-8 flex flex-col gap-3 bg-zinc-100">
       <p className="text-5xl">{country.name}</p>
       <p>Capital: {country.capital}</p>
       <p>Population: {country.population} People</p>
