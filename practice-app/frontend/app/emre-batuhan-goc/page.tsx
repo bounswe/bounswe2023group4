381668292dbc297b8ba406017308843512736446
YigitSekerci <yigitsekerci6174@gmail.com>
2023-05-12 13:32:01 +0300

    make simple country page

--- practice-app/frontend/app/emre-batuhan-goc/page.tsx
+++ practice-app/frontend/app/emre-batuhan-goc/page.tsx
@@ -1,19 +1,14 @@
-import { Country } from '@/api/emre-batuhan-goc/types'
+'use client'
+import { useCountries } from '@/api/emre-batuhan-goc/useCountries'
 import CountryCard from '@/components/emre-batuhan-goc/CountryCard'
 
-const MockCountries: Country[] = Array<Country>(100).fill({
-  name: 'Turkey',
-  capital: 'Ankara',
-  population: 100000000,
-  gdp: 1000000000000,
-})
-
 const Page = () => {
+  const { data } = useCountries()
+
   return (
     <div className="grid gap-3 grid-cols-3 px-4 pt-2">
-      {MockCountries.map((country, i) => (
-        <CountryCard key={i} country={country} />
-      ))}
+      {data &&
+        data.map((country, i) => <CountryCard key={i} country={country} />)}
     </div>
   )
 }
