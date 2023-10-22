7b71485df88c7903ba9619355044349979ab53ef
Selin Işık <56879777+selinisik@users.noreply.github.com>
2023-10-23 02:10:35 +0300

    updated sign-in page ui

--- prediction-polls/frontend/src/Pages/Auth/SignIn/index.jsx
+++ prediction-polls/frontend/src/Pages/Auth/SignIn/index.jsx
@@ -1,147 +1,149 @@
-import React from 'react';
-import { Button, Form, Input } from 'antd';
-import { Link } from 'react-router-dom';
-const bodyStyle = {
-  position: "Absolute",
-  display: 'flex',
-  flexDirection: 'row',
-  width: '100%',
-  height: '100%',
-  overflow: 'hidden',
+import React from "react";
+import { Button, Input, Form, Divider, Typography } from "antd";
+import { ReactComponent as Logo } from "../../../Assets/Logo.svg";
+import { ReactComponent as SignPageAnimation } from "../../../Assets/SignPageAnimation.svg";
+import { ReactComponent as GoogleLogo } from "../../../Assets/icons/GoogleIcon.svg";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+const { Text } = Typography;
+
+const splitContainerStyle = {
+  display: "flex",
+  width: "100%",
+  height: "100vh",
+  margin: "0 auto",
 };
 
-const columnStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  width: '50%',
-  height: '100%',
-  textAlign: 'center',
-}
+const formContainerStyle = {
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  padding: "20px",
+};
+const animationStyle = {
+  maxWidth: "100%",
+  maxHeight: "100%",
+};
+
+const dividerStyle = {
+  color: "#9EC8EE",
+  borderColor: "#9EC8EE",
+};
 
 const imageStyle = {
-  width: '50%',
-  height: '100%',
-}
-const predictionPollStyle = {
-  fontFamily: "Times New Roman",
-  fontSize: 34,
-  fontWeight: 1000,
-  marginTop: '5%',
-  marginBottom: '5%',
-}
+  flex: 1,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  background: "linear-gradient(#EAF3FB, #BEDAF4)",
+};
 
-const signinWithGoogle = {
-  display: 'flex',
-  paddingLeft: '8%',
-  flexDirection: 'row',
-  alignItems: 'center',
-  borderColor: 'black',
-  backgroundColor: 'blue',
-  color: 'black',
-  borderStyle: 'solid',
-  borderWidth: '5%',
-  width: '60%',
-  height: '8%',
-  marginBottom: '5%',
-  marginLeft: '20%',
-  fontWeight: 500,
-  color: 'white',
-}
-const signinwithGoogleText = {
-  marginLeft: '22%',
-  fontSize: 18,
-}
-const separator = {
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-}
-const horizontalLine = {
-  width: '30%',
-  height: '0%',
-  color: 'blue',
-
-}
-const orFormat = {
-  color: 'blue',
-  fontWeight: 700,
-}
-
-const formFomat = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  paddingLeft: '20%',
-  width: '60%',
-  height: '45%',
-}
-const textArea = {
-  width: '120%',
-  height: '100%',
-  fontSize: 22,
-  fontFamily: 'Times New Roman',
-  marginTop: '3%',
-  marginBottom: '3%',
-}
-const labelFormat = {
-  width: '120%',
-  textAlign: 'left',
-  fontWeight: 600,
-  fontSize: 18,
-}
-const forgotPassword = {
-  width: '120%',
-  textAlign: 'right',
-  fontWeight: 600,
-  fontSize: 14
-}
-
-const logIn = {
-  height: '10%',
-  width: '72%',
-  fontWeight: 500,
-  textAlign: 'center',
-  alignItems: 'center',
-  backgroundColor: 'blue',
-  color: 'white',
-  marginLeft: '14%'
-}
-const cantLoginFormat = {
-  fontWeight: 600,
-  fontSize: 18
-}
+const formItemLayout = {
+  labelCol: { span: 24 },
+  wrapperCol: { span: 24 },
+};
+const displayCenterStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
+const formButtonStyle = {
+  ...displayCenterStyle,
+  padding: "22px 0px",
+  width: "100%",
+  background: "#2D87DA",
+  color: "#FFFFFF",
+};
+const logoStyle = {
+  ...displayCenterStyle,
+  marginBottom: "20px",
+  maxWidth: "80%",
+  maxHeight: "80%",
+};
+const googleLogoStyle = {
+  marginRight: "20px",
+};
+const forgotPasswordStyle = {
+  color: "#363A3D",
+};
+const signUpStyle = {
+  color: "#2D87DA",
+};
+const passwordDivStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-end",
+};
+const labelStyle = {
+  fontSize: "12px",
+};
 function SignIn() {
+  const [passwordVisible, setPasswordVisible] = React.useState(false);
+  const navigate = useNavigate();
   return (
-    <body>
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css"></link>
-      <div style={bodyStyle}>
-        <div style={columnStyle}>
-          <p style={predictionPollStyle}>Prediction Poll</p>
-          <Button style={signinWithGoogle}><i class="fab fa-google fa-1x"></i>
-            <p style={signinwithGoogleText}> Sign in with Google </p></Button>
-          <div style={separator}>
-            <hr style={horizontalLine} />
-            <p style={orFormat}>OR</p>
-            <hr style={horizontalLine} /></div>
-          <div style={formFomat}>
-            <label style={labelFormat}>EMAIL ADDRESS</label>
-            <Input style={textArea} type="email" placeholder="example@outlook.com" />
-            < label style={labelFormat}>PASSWORD</label>
-            <Input style={textArea} placeholder="*****************" />
-            <p style={forgotPassword}>FORGOT PASSWORD?</p>
-          </div>
-          <Button style={logIn}>LOG IN</Button>
-          <p style={cantLoginFormat}>CAN'T LOG IN?</p>
-        </div>
-        <div style={imageStyle}>
-          <img
-            src="https://cdn.discordapp.com/attachments/887354522324852759/1165272221317611551/image.png?ex=65463f53&is=6533ca53&hm=7d6045db42bbb9a68f7cf30241535ded3c6caefb8ddc9c26001781c75bf064a6&"
-            alt="Sign-Up Image"
-            style={{ width: '100%', height: '100%' }}
-          />
-        </div>
+    <div style={splitContainerStyle}>
+      <div style={formContainerStyle}>
+        <Link to="/home" style={logoStyle}>
+          <Logo/>
+        </Link>
+        <Form {...formItemLayout}>
+          <Form.Item>
+            <div>
+              <Button style={formButtonStyle}>
+                <GoogleLogo style={googleLogoStyle} />
+
+                <span>Sign In with Google</span>
+              </Button>
+            </div>
+          </Form.Item>
+          <Form.Item>
+            <Divider style={dividerStyle} orientation="center" plain>
+              or
+            </Divider>
+          </Form.Item>
+          <Form.Item name="email">
+            <Text style={labelStyle}>EMAIL ADRESS</Text>
+            <Input
+              size="large"
+              type="email"
+              placeholder="example@outlook.com"
+            />
+          </Form.Item>
+          <Form.Item name="password">
+            <Text style={labelStyle}>PASSWORD</Text>
+            <div style={passwordDivStyle}>
+              <Input.Password
+                size="large"
+                placeholder="password"
+                visibilityToggle={{
+                  visible: passwordVisible,
+                  onVisibleChange: setPasswordVisible,
+                }}
+              />
+              <Button type="link" style={forgotPasswordStyle}>
+                Forgot Password?
+              </Button>
+            </div>
+          </Form.Item>
+          <Form.Item>
+            <Button style={formButtonStyle}>LOG IN</Button>
+          </Form.Item>
+          <Form.Item>
+            <div style={displayCenterStyle}>
+              <span>Don't have an account?</span>
+              <Button type="link" style={signUpStyle} onClick={() => navigate("/auth/sign-up")}>
+                Sign Up
+              </Button>
+            </div>
+          </Form.Item>
+        </Form>
       </div>
-    </body>
+      <div style={imageStyle}>
+        <SignPageAnimation style={animationStyle} />
+      </div>
+    </div>
   );
 }
 
