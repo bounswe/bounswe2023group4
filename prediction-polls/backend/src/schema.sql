f2d9be04c31348a549eb69a3ff2c1ea49ad12b27
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2023-10-22 18:35:35 +0300

    add refresh token schema

--- prediction-polls/backend/src/schema.sql
+++ prediction-polls/backend/src/schema.sql
@@ -2,7 +2,17 @@ CREATE DATABASE predict_app;
 USE predict_app;
 
 CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE refresh_tokens (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    token VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    revoked BOOLEAN NOT NULL DEFAULT 0,
+    UNIQUE KEY token (token),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
