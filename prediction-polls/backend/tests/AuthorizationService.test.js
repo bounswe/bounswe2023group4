f33687dacbf204d13e09a30f02ee6e8390312367
EmreBatuhan <93476131+EmreBatuhan@users.noreply.github.com>
2023-10-22 22:21:51 +0300

    Update tests so that they refer app.js

--- prediction-polls/backend/tests/AuthorizationService.test.js
+++ prediction-polls/backend/tests/AuthorizationService.test.js
@@ -1,7 +1,9 @@
-const app = require('../src/controllers/AuthorizationController')
+const app = require('../src/app.js')
 const request = require('supertest')
 
 test('test if we get accessToken at login', async () => {
-    const response = await request(app).post('/login').send({"name" : "testUsername"})
-    expect(response.body.accessToken).not.toBe(undefined) 
+    const response = await request(app).post('/login').send({"username" : "testUsername","password": "asdadasd"})
+    expect(response.body.accessToken).not.toBe(undefined)
 })
+
+
