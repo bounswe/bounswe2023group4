77f8f440b0ef7afc72d95edbc127b5631079eebe
Selin Işık <56879777+selinisik@users.noreply.github.com>
2023-10-22 19:07:34 +0300

    added example usage of Theme Context

--- prediction-polls/frontend/src/Pages/Home/index.jsx
+++ prediction-polls/frontend/src/Pages/Home/index.jsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import { Button } from 'antd';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Colors } from "../../theme/color";
+import { ThemeContext } from "../../contexts/ThemeContext";
+
 function Home() {
+  const { theme, setTheme } = useContext(ThemeContext);
   const containerStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: '20px',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "20px",
+    backgroundColor: Colors[theme].neutural.white,
+    height: "100vh",
   };
   const buttonContainerStyle = {
-    marginTop: '20px',
+    marginTop: "20px",
+    backgroundColor: Colors[theme].primary[200],
+    color: Colors[theme].neutural.black,
+  };
+  const header = {
+    color: Colors[theme].neutural.black,
+  }
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
   };
 
   return (
     <div style={containerStyle}>
-      <h1>Home Page</h1>
+      <h1 style={header}>Home Page</h1>
       <Link to="/auth/sign-up">
-        <Button type='primary'>Go to Sign Up Page</Button>
+        <Button style={buttonContainerStyle}>Go to Sign Up Page</Button>
       </Link>
       <Link to="/auth/sign-in">
-        <Button style={buttonContainerStyle} type='dashed'>Go to Sign In Page</Button>
+        <Button style={buttonContainerStyle} type="dashed">
+          Go to Sign In Page
+        </Button>
       </Link>
+      <Button style={buttonContainerStyle} onClick={toggleTheme}>
+        Theme
+      </Button>
     </div>
   );
 }
