c953409332b293b8386b770472f9342bbd277dda
EmreBatuhan <93476131+EmreBatuhan@users.noreply.github.com>
2023-10-27 22:06:20 +0300

    Add Poll service without logic

--- prediction-polls/backend/src/repositories/PollDB.js
+++ prediction-polls/backend/src/repositories/PollDB.js
@@ -8,3 +8,52 @@ const pool = mysql.createPool({
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
 }).promise()
+
+
+async function getDiscretePolls(){
+    const sql = 'SELECT * FROM discrete_polls';
+    
+
+    return pool.query(sql).then(([rows]) => {
+        console.log("ROWS")
+        console.log(rows);
+        return rows;
+    }, (err) => {
+        console.log(err);
+        return undefined
+    })
+}
+
+async function getContinuousPolls(){
+
+}
+
+async function getDiscretePollWithId(){
+
+}
+
+async function getContinuousPollWithId(){
+
+}
+
+async function addDiscretePoll(){
+
+}
+
+async function addContinuousPoll(){
+
+}
+
+async function voteDiscretePoll(){
+    
+}
+
+async function voteContinuousPoll(){
+    
+}
+
+
+
+module.exports = {getDiscretePolls, getContinuousPolls, getDiscretePollWithId, getContinuousPollWithId, 
+    addDiscretePoll,addContinuousPoll, voteDiscretePoll, voteContinuousPoll}
+    
--- prediction-polls/backend/src/services/PollService.js
+++ prediction-polls/backend/src/services/PollService.js
@@ -1,17 +1,45 @@
-function getDiscretePolls(){
+const db = require("../repositories/PollDB.js");
+
+async function getDiscretePolls(req,res){
+    try{
+    const result_polls = await db.getDiscretePolls();
+    console.log("result_polls")
+    console.log(result_polls);
+    res.json(result_polls)
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+function getDiscretePollWithId(req,res){
 
 }
 
-function addDiscretePoll(){
+function addDiscretePoll(req,res){
 
 }
 
-function getContinuousPolls(){
+function getContinuousPolls(req,res){
 
 }
 
-function addContinuousPoll(){
+function getContinuousPollWithId(req,res){
 
 }
 
-module.exports = {getDiscretePolls,addDiscretePoll,getContinuousPolls,addContinuousPoll}
+function addContinuousPoll(req,res){
+
+}
+
+function voteDiscretePoll(req,res){
+
+}
+
+function voteContinuousPoll(req,res){
+
+}
+
+module.exports = {getDiscretePolls,getDiscretePollWithId,addDiscretePoll,getContinuousPolls,
+    getContinuousPollWithId,addContinuousPoll,voteDiscretePoll,voteContinuousPoll}
