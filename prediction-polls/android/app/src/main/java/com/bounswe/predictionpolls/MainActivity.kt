3d6f8b5c1a2cd7c3aee7357fb1ff41327e43a21a
YigitSekerci <yigitsekerci6174@gmail.com>
2023-10-28 15:18:18 +0300

    add entry point

--- prediction-polls/android/app/src/main/java/com/bounswe/predictionpolls/MainActivity.kt
+++ prediction-polls/android/app/src/main/java/com/bounswe/predictionpolls/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.navigation.compose.rememberNavController
 import com.bounswe.predictionpolls.ui.main.MAIN_ROUTE
 import com.bounswe.predictionpolls.ui.main.mainScreen
 import com.bounswe.predictionpolls.ui.theme.PredictionPollsTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
