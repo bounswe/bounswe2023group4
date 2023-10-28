cf968e787b8c0623659c1d55183d7441d5fbd854
YigitSekerci <yigitsekerci6174@gmail.com>
2023-10-28 14:50:35 +0300

    add auth service parameters

---
+++ prediction-polls/android/app/src/main/java/com/bounswe/predictionpolls/data/remote/request/SignupRequest.kt
@@ -0,0 +1,6 @@
+package com.bounswe.predictionpolls.data.remote.request
+
+data class SignupRequest(
+    val username: String,
+    val password: String
+)
--- prediction-polls/android/app/src/main/java/com/bounswe/predictionpolls/data/remote/services/AuthService.kt
+++ prediction-polls/android/app/src/main/java/com/bounswe/predictionpolls/data/remote/services/AuthService.kt
@@ -1,20 +1,33 @@
 package com.bounswe.predictionpolls.data.remote.services
 
+import com.bounswe.predictionpolls.data.remote.request.LoginRequest
+import com.bounswe.predictionpolls.data.remote.request.LogoutRequest
+import com.bounswe.predictionpolls.data.remote.request.RefreshAccessTokenRequest
+import com.bounswe.predictionpolls.data.remote.request.SignupRequest
 import com.bounswe.predictionpolls.data.remote.response.LoginResponse
 import com.bounswe.predictionpolls.data.remote.response.RefreshAccessTokenResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthService {
     @POST("/signup")
-    suspend fun signup(): Response<Void>
+    suspend fun signup(
+        @Body signupRequest: SignupRequest
+    ): Response<Void>
 
     @POST("/login")
-    suspend fun login(): Response<LoginResponse>
+    suspend fun login(
+        @Body loginRequest: LoginRequest
+    ): Response<LoginResponse>
 
     @POST("/logout")
-    suspend fun logout(): Response<Void>
+    suspend fun logout(
+        @Body logoutRequest: LogoutRequest
+    ): Response<Void>
 
     @POST("/access-token")
-    suspend fun refreshAccessToken(): Response<RefreshAccessTokenResponse>
+    suspend fun refreshAccessToken(
+        @Body refreshAccessTokenRequest: RefreshAccessTokenRequest
+    ): Response<RefreshAccessTokenResponse>
 }
