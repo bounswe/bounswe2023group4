27967364fb44c5bd2d46447a6a2aa15ed4e350c2
Selin Işık <56879777+selinisik@users.noreply.github.com>
2023-10-29 00:56:35 +0300

    added all pages to the router

--- prediction-polls/frontend/src/Routes/Router.jsx
+++ prediction-polls/frontend/src/Routes/Router.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
 import SignIn from "../Pages/Auth/SignIn";
 import SignUp from "../Pages/Auth/SignUp";
+import Feed from "../Pages/Feed";
+import Create from "../Pages/Create";
+import Leaderboard from "../Pages/Leaderboard";
+import Moderation from "../Pages/Moderation";
+import Notifications from "../Pages/Notifications";
+import Profile from "../Pages/Profile";
+import Settings from "../Pages/Settings";
+import Vote from "../Pages/Vote";
 
 function AppRouter() {
   return (
@@ -16,6 +20,14 @@ function AppRouter() {
         <Route path="/home" element={<Home />} />
         <Route path="/auth/sign-up" element={<SignUp />} />
         <Route path="/auth/sign-in" element={<SignIn />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/moderation" element={<Moderation />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/vote" element={<Vote />} />
       </Routes>
     </BrowserRouter>
   );
