6c57465765463462fb941acef0f3c998941516e2
EmreBatuhan <93476131+EmreBatuhan@users.noreply.github.com>
2023-10-29 11:17:08 +0300

    Fix delete refresh token

--- prediction-polls/backend/src/repositories/AuthorizationDB.js
+++ prediction-polls/backend/src/repositories/AuthorizationDB.js
@@ -34,15 +34,18 @@ async function checkRefreshToken(token){
 }
 
 //Delete the given refresh token from db
-function deleteRefreshToken(token){
+async function deleteRefreshToken(token){
     const sql = 'DELETE FROM refresh_tokens WHERE token = ?';
     const values = [token];
 
+    const [result] = await pool.query(sql, values);
+    return result.affectedRows > 0;
+    /*
     return pool.query(sql, values).then(([result]) => {
         return result.affectedRows > 0;
     }, () => {
         return false
-    })
+    })*/
 
 }
 
