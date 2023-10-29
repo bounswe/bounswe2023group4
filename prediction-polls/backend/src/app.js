f14bc88a596862dc14cc9b36b083dda6720576ea
EmreBatuhan <93476131+EmreBatuhan@users.noreply.github.com>
2023-10-29 11:16:26 +0300

    Change port so It does not conflict frontend

--- prediction-polls/backend/src/app.js
+++ prediction-polls/backend/src/app.js
@@ -19,7 +19,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
