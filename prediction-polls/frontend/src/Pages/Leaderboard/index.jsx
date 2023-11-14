94671d6e6f9288ce6d11d4751537e60d1c2c24b6
kutaysaran <74209499+kutaysaran@users.noreply.github.com>
2023-11-14 13:46:31 +0300

    point button

--- prediction-polls/frontend/src/Pages/Leaderboard/index.jsx
+++ prediction-polls/frontend/src/Pages/Leaderboard/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Menu from '../../Components/Menu'
 import styles  from './Leaderboard.module.css'
+import PointsButton from "../../Components/PointsButton";
+import pointData from "../../MockData/PointList.json"
 
 function Leaderboard() {
   return (
     <div className={styles.page}>
     <Menu currentPage="Leaderboard" />
     <h1>Leaderboard Page</h1>
+    <PointsButton points={pointData.points}/>
+
  </div>
   )
 }
