be8edec531b608d36eff3bc63b405a1d36904af6
EmreBatuhan <93476131+EmreBatuhan@users.noreply.github.com>
2023-11-19 23:21:19 +0300

    Added logic to profile classes

--- prediction-polls/backend/src/repositories/ProfileDB.js
+++ prediction-polls/backend/src/repositories/ProfileDB.js
@@ -1,3 +1,4 @@
+const authDb = require("../repositories/AuthorizationDB.js");
 const mysql = require('mysql2')
 
 require('dotenv').config();
@@ -11,10 +12,14 @@ const pool = mysql.createPool({
 
 
 async function getProfileWithProfileId(profileId){
+    console.log("profileId")
+    console.log(profileId)
+
     const sql = 'SELECT * FROM profiles WHERE id= ?';
 
     try {
-        const [rows, fields] = await pool.query(sql, [profileId]);
+        const [rows] = await pool.query(sql, [profileId]);
+        console.log(rows)
         return rows;
     } catch (error) {
         console.error('getProfileWithProfileId(): Database Error');
@@ -27,67 +32,105 @@ async function getProfileWithUserId(userId){
 
     try {
         const [rows, fields] = await pool.query(sql, [userId]);
-        return rows;
+        return {profile:rows[0]};
     } catch (error) {
-        console.error('getProfileWithUserId(): Database Error');
-        throw error;
+        return {error:{code:1000,message:"An error occured:" + error}};
     }
 }
 
 async function getProfileWithUserInfo({username,email}){
-    const userId=0;
-    if(username){
-        const sql = 'SELECT * FROM users WHERE username = ?';
-        //TODO retrieve user id using username
-        userId = 1;
-    }
-    else{
-        const sql = 'SELECT * FROM users WHERE email = ?';
-        //TODO retrieve user id using email
-        userId = 1;
-    }
+    const userId = await authDb.findUserId({username,email});
     const user = await getProfileWithUserId(userId);
     return user;
 }
 
 async function addProfile({
-    user_id,
+    userId,
     username ,
     profile_picture,
     biography,
     birthday ,
-    is_hidden}){
+    isHidden}){
 
-    const sql = 'INSERT INTO profiles (user_id, username, profile_picture_data, biography, birthday, is_hidden) VALUES (?, ?, ?, ?, ?, ?)';
-    values = [user_id,username,profile_picture,biography,birthday,is_hidden]
-        
-    const [resultSetHeader] = await pool.query(sql_poll, values);
+    try {
+        const sql = 'INSERT INTO profiles (user_id, username, profile_picture_data, biography, birthday, is_hidden) VALUES (?, ?, ?, ?, ?, ?)';
+        values = [userId,username,profile_picture,biography,birthday,isHidden]
+            
+        const [resultSetHeader] = await pool.query(sql, values);
         profile_id = resultSetHeader.insertId;
         if (!profile_id) {
-            return false;
+            return {error:{code:1111,message:"Could not insert"}};
         }
-        return profile_id;
+        return {profile_id:profile_id};
+    }catch(error){
+        // Error management will be handled later
+        /*
+        console.log(error);
+        if(error.sqlMessage == "Error: Column 'user_id' cannot be null"){
+            return {error:{code:1111,message:"Gimme user id"}}
+        }
+        if(error.sqlMessage == "Error: Cannot add or update a child row: a foreign key constraint "+
+        "fails (`predict_app`.`profiles`, CONSTRAINT `profiles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`))"){
+            return {error:{code:1110,message:"No user exists with the given user id"}}
+        }
+        */
+        return {error:{code:1000,message:"An error occured:" + error}};
+    }
+
 }
 
 async function updateProfile({
-    user_id,
+    userId,
     username ,
     profile_picture,
     biography,
     birthday ,
-    is_hidden}){
+    isHidden}){
 
-    if(profile_picture){
+    let user_id = userId
 
+    if(!userId){
+        user_id = await authDb.findUserId({username});
     }
-    if(biography){
+    try {
+        if(profile_picture){
+            const sql = 'UPDATE profiles SET profile_picture_data = ? WHERE user_id = ?';
+            values = [profile_picture,user_id];
 
-    }
-    if(birthday){
+            const [resultSetHeader] = await pool.query(sql, values);
+        }
+        if(biography){
+            const sql = 'UPDATE profiles SET biography = ? WHERE user_id = ?';
+            values = [biography,user_id];
 
-    }
-    if(is_hidden){
-        
-    }
+            const [resultSetHeader] = await pool.query(sql, values);
+        }
+        if(birthday){
+            const sql = 'UPDATE profiles SET birthday = ? WHERE user_id = ?';
+            values = [birthday,user_id];
+
+            const [resultSetHeader] = await pool.query(sql, values);
+        }
+        if(isHidden){
+            const sql = 'UPDATE profiles SET is_hidden = ? WHERE user_id = ?';
+            values = [isHidden,user_id];
+
+            const [resultSetHeader] = await pool.query(sql, values);
+        }
+        console.log("Im done");
+        return {status:"success"};
+    }catch(error) {
+        return {error:{code:1000,message:"An error occured:" + error}};
+    } 
 }
+
+
+async function getProfileIsHidden(profileId){
+    profile = await getProfileWithProfileId(profileId);
+    return profile.is_hidden;
+
+}
+
+
+module.exports = {getProfileWithProfileId,getProfileWithUserId,getProfileWithUserInfo,addProfile,updateProfile}
     
--- prediction-polls/backend/src/services/ProfileService.js
+++ prediction-polls/backend/src/services/ProfileService.js
@@ -1,20 +1,43 @@
-const db = require("../repositories/PollDB.js");
+const db = require("../repositories/ProfileDB.js");
 
 
-function getProfile(req,res){
-    
+async function getProfile(req,res){
+    const { userId, username, email } = req.query;
+    if(userId){
+        console.log("Found UserId");
+        const profile = await db.getProfileWithUserId(userId);
+        return res.status(200).json(profile);
+    }
+    console.log("Did not find UserId");
+    const profile = await db.getProfileWithUserInfo({username,email})
+    res.status(200).json(profile);
 }
 
-function getProfileWithProfileId(req,res){
-    
+async function getProfileWithProfileId(req,res){
+    const profileId = req.params.profileId;
+    const {profile,error} = await db.getProfileWithProfileId(profileId);
+    if(error){
+        return res.status(400).json({error:error});
+    }
+    res.status(200).json(profile);
 }
 
-function addProfile(req,res){
-    
+async function addProfile(req,res){
+    const {userId,username,email,profile_picture,biography, birthday, isHidden} = req.body;
+    const {profile_id,error} = await db.addProfile({userId,username,email,profile_picture,biography, birthday, isHidden});
+    if(error){
+        return res.status(400).json({error:error});
+    }
+    return res.status(200).json({profileId:profile_id});
 }
 
-function updateProfile(req,res){
-    
+async function updateProfile(req,res){
+    const {userId,username,profile_picture,biography, birthday, isHidden} = req.body;
+    const {status,error} = await db.updateProfile({userId,username,profile_picture,biography, birthday, isHidden});
+    if(error){
+        return res.status(400).json({error:error});
+    }
+    return res.status(200).json({status:status});
 }
 
 module.exports = {getProfile,getProfileWithProfileId,addProfile,updateProfile}
