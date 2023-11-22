ce615599dbac1e941f9630bd227e201ba2ad54cb
Ahmet Emre Safak <aemresafak@gmail.com>
2023-11-22 19:54:32 +0300

    create feed ui state

---
+++ prediction-polls/android/app/src/main/java/com/bounswe/predictionpolls/ui/feed/FeedUiState.kt
@@ -0,0 +1,15 @@
+package com.bounswe.predictionpolls.ui.feed
+
+import com.bounswe.predictionpolls.domain.poll.Poll
+
+sealed interface FeedUiState {
+    data object Loading : FeedUiState
+    data class HasFeed(val feed: List<Poll>) : FeedUiState
+
+    /**
+     * Represents the case where there is no feed to show. This case is practically equivalent to HasFeed(emptyList()) but is used to make the code more readable.
+     */
+    data object NoFeed : FeedUiState
+
+    data class Error(val message: String) : FeedUiState
+}
--- prediction-polls/android/app/src/main/java/com/bounswe/predictionpolls/ui/feed/FeedViewModel.kt
+++ prediction-polls/android/app/src/main/java/com/bounswe/predictionpolls/ui/feed/FeedViewModel.kt
@@ -1,10 +1,11 @@
 package com.bounswe.predictionpolls.ui.feed
 
 import androidx.lifecycle.ViewModel
+import com.bounswe.predictionpolls.domain.feed.GetFeedUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class FeedViewModel @Inject constructor() : ViewModel() {
+class FeedViewModel @Inject constructor(private val getFeedUseCase: GetFeedUseCase) : ViewModel() {
 
 }
