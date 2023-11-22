901a576d0a91db2563218be49968458eae50b916
Ahmet Emre Safak <aemresafak@gmail.com>
2023-11-22 20:38:13 +0300

    update feed screen nav graph

--- prediction-polls/android/app/src/main/java/com/bounswe/predictionpolls/domain/feed/GetFeedUseCase.kt
+++ prediction-polls/android/app/src/main/java/com/bounswe/predictionpolls/domain/feed/GetFeedUseCase.kt
@@ -2,12 +2,22 @@ package com.bounswe.predictionpolls.domain.feed
 
 import com.bounswe.predictionpolls.common.Result
 import com.bounswe.predictionpolls.domain.poll.Poll
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 import javax.inject.Inject
 
 class GetFeedUseCase @Inject constructor(
     private val feedRepository: FeedRepository
 ) {
-    suspend operator fun invoke(page: Int): Result<List<Poll>> {
-        return feedRepository.getPolls(page)
-    }
+    suspend operator fun invoke(page: Int): Result<ImmutableList<Poll>> =
+        when (val result = feedRepository.getPolls(page)) {
+            is Result.Success -> {
+                Result.Success(result.data.toImmutableList())
+            }
+
+            is Result.Error -> {
+                Result.Error(result.exception)
+            }
+        }
+
 }
