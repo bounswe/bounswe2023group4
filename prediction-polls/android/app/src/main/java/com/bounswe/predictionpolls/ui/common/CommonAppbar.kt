2f046c61c2ec14998e0edbf5842cc3a529ea54a2
YigitSekerci <yigitsekerci6174@gmail.com>
2023-11-26 16:40:40 +0300

    add cd to the app bar

--- prediction-polls/android/app/src/main/java/com/bounswe/predictionpolls/ui/common/CommonAppbar.kt
+++ prediction-polls/android/app/src/main/java/com/bounswe/predictionpolls/ui/common/CommonAppbar.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bounswe.predictionpolls.R
@@ -26,7 +27,7 @@ fun CommonAppbar(
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 8.dp),
+            .padding(horizontal = 8.dp, vertical = 8.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
@@ -37,8 +38,8 @@ fun CommonAppbar(
         ) {
             Icon(
                 painter = painterResource(id = R.drawable.ic_hamburger),
-                contentDescription = null,
-                modifier = Modifier.size(40.dp)
+                contentDescription = stringResource(id = R.string.cd_menu),
+                modifier = Modifier.size(32.dp)
             )
         }
         IconButton(
@@ -48,8 +49,8 @@ fun CommonAppbar(
         ) {
             Icon(
                 painter = painterResource(id = R.drawable.ic_notification),
-                contentDescription = null,
-                modifier = Modifier.size(40.dp)
+                contentDescription = stringResource(id = R.string.cd_notification),
+                modifier = Modifier.size(32.dp)
             )
         }
     }
