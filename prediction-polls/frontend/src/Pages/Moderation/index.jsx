8dffb77005884f724b036f308c38a8355b18dbc8
Enes Furkan Arslan <enfurars@gmail.com>
2023-12-07 18:39:59 +0300

    UI of not moderator
    UI of moderation page is implemented for users that are not currently moderators

--- prediction-polls/frontend/src/Pages/Moderation/index.jsx
+++ prediction-polls/frontend/src/Pages/Moderation/index.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import Menu from '../../Components/Menu'
-import styles from './Moderation.module.css'
+// Moderation.js
+import React from 'react';
+import Menu from '../../Components/Menu';
+import styles from './Moderation.module.css';
+import { Button } from 'antd'; 
 
 function Moderation() {
+  const isModerator = false;
+
+  const handleBecomeModerator = () => {
+    console.log('User wants to become a moderator');
+  };
+
   return (
     <div className={styles.page}>
-    <Menu currentPage="Moderation" />
-    <h1>Moderation Page</h1>
- </div>
-  )
+      <Menu currentPage="Moderation" />
+      {isModerator ? (
+        <div>You are already a moderator.</div>
+      ) : (
+        <>
+          <div className={styles.questionCard}>
+            <p className={styles.text}>Would you like to apply to become a moderator?</p>
+            <Button className={styles.btn} type="primary" onClick={handleBecomeModerator}>
+            Apply 
+          </Button> 
+          </div> 
+          
+        </>
+        
+      )}
+    </div>
+  );
 }
 
 export default Moderation;
