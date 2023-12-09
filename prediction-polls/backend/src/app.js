1ac05009bb2f61962a4b2428cec2f0ec33400d5c
EmreBatuhan <93476131+EmreBatuhan@users.noreply.github.com>
2023-12-09 23:30:32 +0300

    Add new routes to app

--- prediction-polls/backend/src/app.js
+++ prediction-polls/backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authRouter = require('./routes/AuthorizationRouter.js'); 
 const pollRouter = require('./routes/PollRouter.js');
 const profileRouter = require('./routes/ProfileRouter.js');
+const moderatorRouter = require('./routes/ModeratorRouter.js');
 const tagRoutine = require('./routines/tagRoutine.js');
 
 const cors = require("cors");
@@ -22,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use('/polls',pollRouter);
 app.use('/auth', authRouter);
 app.use('/profiles', profileRouter);
+app.use('/moderators', profileRouter);
 
 const swaggerSpec = swaggerJsDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
