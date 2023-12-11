bdc36f32e7748421850c6e62c8e8d2ebf90b4e32
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2023-12-11 22:06:18 +0300

    fix typo

--- prediction-polls/annotations/src/services/AddContextService.js
+++ prediction-polls/annotations/src/services/AddContextService.js
@@ -3,7 +3,7 @@ const url = require('url');
 async function attachContext(req, res, next) {
   req.body["@context"] = "http://www.w3.org/ns/anno.jsonld";
   if (typeof req.body.target === 'string') {
-    req.body.target = url.resolve(process.env.APP_URI, req.body.target);;
+    req.body.target = url.resolve(process.env.APP_URI, req.body.target);
   } else if (typeof req.body.target === 'object') {
     req.body.target.source = url.resolve(process.env.APP_URI, req.body.target.source);
   }
