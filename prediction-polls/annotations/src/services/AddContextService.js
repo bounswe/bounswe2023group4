b9230766126862a3bc979b9d6c99b374e8c054bd
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2023-12-11 22:05:57 +0300

    concatenate url properly

--- prediction-polls/annotations/src/services/AddContextService.js
+++ prediction-polls/annotations/src/services/AddContextService.js
@@ -1,9 +1,11 @@
+const url = require('url');
+
 async function attachContext(req, res, next) {
   req.body["@context"] = "http://www.w3.org/ns/anno.jsonld";
   if (typeof req.body.target === 'string') {
-    req.body.target = process.env.APP_URI + req.body.target;
+    req.body.target = url.resolve(process.env.APP_URI, req.body.target);;
   } else if (typeof req.body.target === 'object') {
-    req.body.target.source = process.env.APP_URI + req.body.target.source;
+    req.body.target.source = url.resolve(process.env.APP_URI, req.body.target.source);
   }
   req.body.type = "Annotation";
   next();
