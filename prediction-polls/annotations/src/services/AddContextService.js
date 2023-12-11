8cdae9634f3bb3fa36fffd9bcfd39e308e1dcaad
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2023-12-11 22:03:16 +0300

    add the frontend link

--- prediction-polls/annotations/src/services/AddContextService.js
+++ prediction-polls/annotations/src/services/AddContextService.js
@@ -1,5 +1,10 @@
 async function attachContext(req, res, next) {
   req.body["@context"] = "http://www.w3.org/ns/anno.jsonld";
+  if (typeof req.body.target === 'string') {
+    req.body.target = process.env.APP_URI + req.body.target;
+  } else if (typeof req.body.target === 'object') {
+    req.body.target.source = process.env.APP_URI + req.body.target.source;
+  }
   req.body.type = "Annotation";
   next();
 }
