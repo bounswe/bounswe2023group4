3571717020eb5c8441ed13421b925147b1a566e3
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2023-12-23 15:52:39 +0300

    bring in ObjectID again

--- prediction-polls/annotations/src/services/AnnotationService.js
+++ prediction-polls/annotations/src/services/AnnotationService.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { resolve } = require('url');
 require('dotenv').config();
 
