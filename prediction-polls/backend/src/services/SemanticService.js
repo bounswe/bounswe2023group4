6e07c0454382e4c40356a23768cc21193757d8a7
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2023-12-23 20:49:12 +0300

    implement router service

--- prediction-polls/backend/src/services/SemanticService.js
+++ prediction-polls/backend/src/services/SemanticService.js
@@ -12,3 +12,22 @@ async function searchSemanticTags(keyword) {
 
   return axios.get(url, { params })
 }
+
+async function getTagsForKeyword(req, res) {
+  const keyword = req.query.keyword;
+  const semanticTags = await searchSemanticTags(keyword);
+  console.log(semanticTags.data.search)
+  const searchArray = semanticTags.data.search;
+  if (searchArray) {
+    const responseArray = searchArray.map(element => {
+      return { id: element.id, label: element.display.label.value, description: element.display.description.value };
+    });
+
+    console.log(responseArray)
+    res.json(responseArray)
+  } else {
+    res.json([])
+  }
+}
+
+module.exports = {getTagsForKeyword}
