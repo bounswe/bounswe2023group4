9a7f3b3f7bf52edbf4d9b1a042fe24ea03a1209e
Ali Nasra <52269552+AliNasra@users.noreply.github.com>
2023-12-25 17:39:03 +0300

    Added the terms of use

--- prediction-polls/frontend/src/Pages/Auth/SignUp/index.jsx
+++ prediction-polls/frontend/src/Pages/Auth/SignUp/index.jsx
@@ -101,15 +101,16 @@ function SignUp() {
         ]}
       >
         <div className={styles.ruleStyle}>
-          <p>Before proceeding to make a decision on the poll in question, please carefully review and agree to the following terms:</p>
-          <p><strong>1. Impartiality:</strong> I affirm that I have no personal or financial interest in the outcomes of this poll and that I have not participated in the poll. I commit to making an unbiased decision based solely on the poll's content and the evidence provided.</p>
-          <p><strong>2. Confidentiality:</strong> I understand that my role as a jury member is a position of trust. I agree not to disclose or discuss any details of the poll or its deliberation process outside the official channels provided by the platform.</p>
-          <p><strong>3. Knowledge and Honesty:</strong> I acknowledge that I am making decisions based on the tags I am interested in and knowledgeable about. I will conduct any necessary research and fact-checking to ensure the accuracy of my decision.</p>
-          <p><strong>4. Respect and Civility:</strong> I commit to engaging respectfully and constructively with the poll and fellow jury members. I understand that my role is to contribute positively to the community and uphold the platform's standards.</p>
-          <p><strong>5. Rule Adherence:</strong> I have read and understood the platform's rules and guidelines for jury members, including the consequences of any misconduct or failure to act responsibly. I agree to adhere to these rules throughout the decision-making process.</p>
-          <p><strong>6. Timely Participation:</strong> I understand the importance of making a timely decision within the 24-hour timeframe and commit to adhering to this schedule to ensure the smooth functioning of the platform.</p>
-          <p><strong>7. Ethical Conduct:</strong> I promise to conduct myself ethically and responsibly, recognizing the impact my decision may have on the poll's creator, participants, and the platform community.</p>
-          <p>By clicking "I Agree," you affirm your understanding and commitment to these terms and the responsibilities of being a jury member. Your thoughtful and fair participation is crucial to maintaining the integrity and quality of our community's decision-making process.</p>
+          <p>Welcome to <strong>Insight Arena</strong>, a dynamic platform dedicated to fostering a community of informed decision-makers through interactive polls and predictions.</p>
+          <p>By creating an account with Insight Arena, I hereby acknowledge and agree to the following terms:</p>
+          <p><strong>1. Privacy and Data Protection:</strong> I agree to the Privacy Policy of Insight Arena and consent to the collection and use of my personal information in accordance with KVKK regulations.</p>
+          <p><strong>2. Conduct and Community Engagement:</strong> I commit to engaging with the community in a respectful and responsible manner. I understand that any violation of community standards may lead to account suspension or termination. I will participate in polls and share my opinions while upholding the standards of Prediction Polls.</p>
+          <p><strong>3. Content Moderation:</strong> If I choose to act as a moderator, I agree to remain impartial and maintain the confidentiality of the poll processes. I confirm that I have selected at least one tag relevant to the poll and have not participated in that specific poll.</p>
+          <p><strong>4. Points and Rewards:</strong> I understand that I will earn Domain Specific Points and General Points based on the accuracy of my predictions and my level of activity on the platform.
+            I acknowledge that the rules governing the points system may change, and I agree to abide by any such amendments.</p>
+          <p><strong>5. Intellectual Property:</strong> I agree that any content I post on Prediction Polls remains my intellectual property. By posting content, I grant Insight Arena a non-exclusive license to use this content within the platform.</p>
+          <p><strong>6. Changes to Terms:</strong> I acknowledge that Prediction Polls may revise these terms at any time, and such changes will be effective upon posting on the platform or notification via email.</p>
+          <p>By clicking "<strong>I Agree!</strong>" you affirm your understanding and commitment to these terms and the responsibilities of being a jury member. Your thoughtful and fair participation is crucial to maintaining the integrity and quality of our community's decision-making process.</p>
         </div>
       </Modal>
       <div className={styles.formContainerStyle}>
