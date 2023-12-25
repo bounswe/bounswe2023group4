a1238f3eb68313cdcbe341fe75954d98b9164e71
Ahmet Emre Safak <aemresafak@gmail.com>
2023-12-25 18:43:32 +0300

    points are shown

--- prediction-polls/android/app/src/main/java/com/bounswe/predictionpolls/ui/common/CommonAppbar.kt
+++ prediction-polls/android/app/src/main/java/com/bounswe/predictionpolls/ui/common/CommonAppbar.kt
@@ -1,26 +1,37 @@
 package com.bounswe.predictionpolls.ui.common
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bounswe.predictionpolls.R
+import com.bounswe.predictionpolls.ui.theme.MontserratFontFamily
 
 @Composable
 fun CommonAppbar(
     isVisible: Boolean = true,
     onMenuClick: () -> Unit = {},
+    points: Int? = null
 ) {
     if (!isVisible) return
 
@@ -43,11 +54,37 @@ fun CommonAppbar(
             )
         }
         Spacer(modifier = Modifier.weight(1f))
+        if (points != null) {
+            PointsDisplay(points = points)
+        }
+    }
+}
+
+@Composable
+fun PointsDisplay(points: Int) {
+    Text(
+        text = "$points GP",
+        modifier = Modifier
+            .clip(RoundedCornerShape(16.dp))
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(8.dp),
+        fontFamily = MontserratFontFamily,
+        fontSize = 20.sp,
+        color = Color.White,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Preview
+@Composable
+private fun PointsDisplayPreview() {
+    Box {
+        PointsDisplay(points = 100)
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun CommonAppbarPreview() {
-    CommonAppbar()
+    CommonAppbar(points = 100)
 }
